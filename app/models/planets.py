7c42d8a16bb8f5719350ee6ff41389c44c0ecdaa
Vicky Farinango <vickyfarinango@gmail.com>
2025-04-24 14:44:06 -0400

    feat: Adds Wave 3 setup

--- app/models/planets.py
+++ app/models/planets.py
@@ -1,17 +1,27 @@
-class Planet: 
-    def __init__(self, id, name, description, atmosphere):
-        self.id = id 
-        self.name = name
-        self.description = description
-        self.atmosphere = atmosphere
 
-planets = [
-    Planet(1, "Mercury", "Highly eccentric orbit", "thin"),
-    Planet(2, "Venus", "Slightly smaller than Earth", "thick"),
-    Planet(3, "Earth", "Third planet from the Sun", "middle"),
-    Planet(4, "Mars", "Nicknamed the Red Planet", "thin"),
-    Planet(5, "Jupiter", "Largest planet", "very thick"),
-    Planet(6, "Saturn", "Has many moons", "thick"),
-    Planet(7, "Uranus", "Ice giant", "thick"),
-    Planet(8, "Neptune", "Dark, cold planet", "thick")
-]
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    atmosphere: Mapped[str]
+    
+# class Planet: 
+#     def __init__(self, id, name, description, atmosphere):
+#         self.id = id 
+#         self.name = name
+#         self.description = description
+#         self.atmosphere = atmosphere
+
+# planets = [
+#     Planet(1, "Mercury", "Highly eccentric orbit", "thin"),
+#     Planet(2, "Venus", "Slightly smaller than Earth", "thick"),
+#     Planet(3, "Earth", "Third planet from the Sun", "middle"),
+#     Planet(4, "Mars", "Nicknamed the Red Planet", "thin"),
+#     Planet(5, "Jupiter", "Largest planet", "very thick"),
+#     Planet(6, "Saturn", "Has many moons", "thick"),
+#     Planet(7, "Uranus", "Ice giant", "thick"),
+#     Planet(8, "Neptune", "Dark, cold planet", "thick")
+# ]
