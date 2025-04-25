24193390f027be690348241a88a99b0ced21cf27
Vicky Farinango <vickyfarinango@gmail.com>
2025-04-25 17:47:34 -0400

    feat: Implements Wave 4 get_one_planet() and validate_planet().

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -12,9 +12,9 @@ def create_planet():
     description = request_body["description"]
     atmosphere = request_body["atmosphere"]
 
-    new_planet = Planet(name=name, description=description, atmosphere=atmosphere)
-    db.session.add(new_planet)
-    db.session.commit()
+    new_planet = Planet(name=name, description=description, atmosphere=atmosphere) #make instance of planet class
+    db.session.add(new_planet) # stage instance to add to the db
+    db.session.commit() # adds instance to the db
 
     planets_response = {
                 "id": new_planet.id,
@@ -31,7 +31,6 @@ def get_all_planets():
     planets = db.session.scalars(query)
 
     # planet = validate_planet(id)
-
     planets_response = []
     for planet in planets:
         planets_response.append(
@@ -59,30 +58,75 @@ def get_all_planets():
 #         )
 #     return planets_response
 
-# @planets_bp.get("/<id>")
-# def get_one_planet(id):
-#     planet = validate_planet(id)
 
-#     planet_dict = dict(
-#         id = planet.id,
-#         title = planet.name,
-#         description = planet.description,
-#         atmosphere = planet.atmosphere
-#     )
 
-#     return planet_dict
 
-# wave 4
-# def validate_planet(id):
-#     try:
-#         id = int(id)
-#     except:
-#         invalid = {"message": f"Planet id ({id}) is invalid."}
-#         abort(make_response(invalid,400))
+# --- WAVE 4 -------
+# https://github.com/AdaGold/solar-system-api/blob/main/project-directions/wave_04.md
 
-#     for planet in planets:
-#         if planet.id == id:
-#             return planet
 
-#     not_found = {"message": f"Planet with id ({id}) was not not found."}
-#     abort(make_response(not_found, 404))
+
+@planets_bp.get("/<id>")
+def get_one_planet(id):
+    planet = validate_planet(id)
+
+    planet_dict = dict(
+        id = planet.id,
+        name = planet.name,
+        description = planet.description,
+        atmosphere = planet.atmosphere
+    )
+
+    return planet_dict
+
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        invalid = {"message": f"Planet id ({id}) is invalid."}
+        abort(make_response(invalid,400))
+
+    #Write SQL to SELECT * FROM planets; --> in SQLAlchemy syntax
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query) # planets is a list
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+
+    not_found = {"message": f"Planet with id ({id}) was not not found."}
+    abort(make_response(not_found, 404))
+
+
+# PATCH(partial) request with valid planet data to update one existing planet and get a 201 response
+@planets_bp.route('/<int:item_id>', methods=['PATCH'])
+def update_one_planet(id):
+    planet = validate_planet(id)
+
+    data = get_all_planets()
+
+    updated_data = request.get_json() #parses incoming request data that is in JSON format and converts to a Python dictionary
+    
+    # How do we access our Planets dictionary (SQL table) 
+    
+    # How do we update the description key to a new value
+
+
+
+
+# 404 for non existing planet and 400 for invalid planet_id
+
+# DELETE request for an existing planet and get a 200 ok response
+# 404 for non existing planet and 400 for invalid planet_id
+
+
+
+@app.route('/data', methods=['GET', 'PATCH'])
+
+def handle_data():
+    if request.method == 'GET':
+        return jsonify(data)
+    elif request.method == 'PATCH':
+        data.update(request.get_json())
+        return jsonify(data)
